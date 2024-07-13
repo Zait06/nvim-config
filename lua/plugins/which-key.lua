ab10f08bad65843e86e8ec07a9e8fe606237ef56
Zait Hernandez <zait-hdez@hotmail.com>
2024-07-13 12:18:18 -0600

    Add wich key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/which-key.nvim",
+    keys = { "<leader>", "<c-r>", "<c-w>", '"', "'", "`", "c", "v", "g" },
+    cmd = "WhichKey",
+    config = function(_, opts)
+      require("which-key").setup(opts)
+    end,
+  },
+}
