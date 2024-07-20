88d6779eb871a7e7f769834e57aa093116cf0ac8
Zait06 <zait-hdez@hotmail.com>
2024-07-19 20:14:11 -0600

    paring characters and new key mappings

--- lua/base/vim-options.lua
+++ lua/base/vim-options.lua
@@ -4,11 +4,12 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set clipboard=unnamedplus")
-
 vim.cmd("set hlsearch")
 vim.cmd("set incsearch")
 vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
+vim.cmd("set splitbelow")
+vim.cmd("set splitright")
 
 vim.g.mapleader = " "
 
@@ -16,7 +17,6 @@ vim.wo.relativenumber = true
 vim.wo.number = true
 
 --Mapping
-
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<C-b>", vim.cmd.NvimTreeToggle)
 vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Move to right window" })
@@ -25,7 +25,9 @@ vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Move to up window" })
 vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Move to down window" })
 vim.keymap.set("n", "<Tab>", vim.cmd.bnext)
 vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious)
-vim.keymap.set("n", "<C-w>b", ":bdelete | :bnext<CR>", { desc = "Delete buffer" })
+vim.keymap.set("n", "<C-w>b", ":bdelete!<CR>:bnext<CR>", { desc = "Delete buffer" })
 vim.keymap.set("n", "//", ":noh<CR>")
 vim.keymap.set("n", "<C-s>v", vim.cmd.vsplit, { desc = "Split vertially" })
 vim.keymap.set("n", "<C-s>h", vim.cmd.split, { desc = "Split horizontally" })
+vim.keymap.set("n", "<C-t>", ":split<CR>:ter<CR>:resize 7<CR>", { desc = "Open terminal" })
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Open terminal" })
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,20 +1,29 @@
 return {
-	{ "hrsh7th/cmp-nvim-lsp" },
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		opts = function()
-			return require("configs.completions")
-		end,
-		config = function(_, opts)
-			require("luasnip.loaders.from_vscode").lazy_load()
-			require("cmp").setup(opts)
-		end,
-	},
+  { "hrsh7th/cmp-nvim-lsp" },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      return require("configs.completions")
+    end,
+    config = function(_, opts)
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup(opts)
+      cmp.event:on("confirm_done", require("nvim-autopairs.completion.cmp").on_confirm_done())
+    end,
+  },
 }
